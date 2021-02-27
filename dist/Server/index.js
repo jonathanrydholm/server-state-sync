58ddfdf01a32a16a3f5a780680a56f307038ee15
mufflan <jonathanrydholm@outlook.com>
2021-02-27 21:02:20 +0100

    removed old version completely

--- dist/Server/index.js
+++
@@ -1,5 +0,0 @@
-import SocketClient from './SocketClient';
-import State from './State';
-import StateManager from './StateManager';
-import StateSyncer from './StateSyncer';
-export { SocketClient, State, StateManager, StateSyncer };
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { StateSyncer } from './Server';
-import Client from './Client';
-import { SYNC_ERROR } from './errorTypes';
-
-export {
-    StateSyncer,
-    SYNC_ERROR,
-    Client
-};
